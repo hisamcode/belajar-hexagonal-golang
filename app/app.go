0853f3c8cb1317cc41836b36a47654958a229fc9
hisamcode <hisamcode@gmail.com>
2021-10-24 23:44:13 +0700

    init

---
+++ app/app.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Start() {
+
+	router := mux.NewRouter()
+
+	// define routes
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	// starting server
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "post request recieved")
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/hisamcode/banking/app"
+
+func main() {
+
+	app.Start()
+
+}
