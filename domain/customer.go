4e626464c034747d5f528699b72e39749de322d2
Hisam Maulana <hisamcode@gmail.com>
2021-10-31 18:20:35 +0000

    feat: add sqlx

--- domain/customer.go
+++ domain/customer.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Customer struct {
-	Id          string
-	Name        string
+	Id          string  `db:"customer_id"`
+	Name        string  
 	City        string
 	Zipcode     string
-	DateOfBirth string
+	DateOfBirth string  `db:"date_of_birth"`
 	Status      string
 }
 
--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -1,82 +1,67 @@
 package domain
 
 import (
-  "database/sql"
-  "time"
+	"database/sql"
+	"time"
 
-  _ "github.com/go-sql-driver/mysql"
-  "github.com/hisamcode/belajar-hexagonal-golang/errs"
-  "github.com/hisamcode/belajar-hexagonal-golang/logger"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/hisamcode/belajar-hexagonal-golang/errs"
+	"github.com/hisamcode/belajar-hexagonal-golang/logger"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type CustomerRepositoryDb struct {
-  client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError) {
+	var err error
+	customers := make([]Customer, 0)
 
-  var rows *sql.Rows
-  var err error
-  
-  if status == "" {
-    findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
-    rows, err = d.client.Query(findAllSql)
-  } else {
-    findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where status = ?"
-    rows, err = d.client.Query(findAllSql, status)
-  }
-  
-  if err != nil {
-    logger.Error("Error while querying customers table " + err.Error())
-    return nil, errs.NewUnexpectedError("Unexpected database error")
-  }
+	if status == "" {
+		findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
+		err = d.client.Select(&customers, findAllSql)
+	} else {
+		findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where status = ?"
+		err = d.client.Select(&customers, findAllSql, status)
+	}
 
-  customers := make([]Customer, 0)
-  for rows.Next() {
-    var c Customer
+	if err != nil {
+		logger.Error("Error while querying customers table " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected database error")
+	}
 
-    err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
-
-    if err != nil {
-      logger.Error("Error while scanning customers table " + err.Error())
-      return nil, errs.NewUnexpectedError("Unexpected database error")
-    }
-
-    customers = append(customers, c)
-  }
-
-  return customers, nil
+	return customers, nil
 }
 
 func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
-  customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
+	customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
 
-  row := d.client.QueryRow(customerSql, id)
+	var c Customer
+	err := d.client.Get(&c, customerSql, id)
 
-  var c Customer
-  err := row.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errs.NewNotFoundError("Customer not found")
+		} else {
+			logger.Error("error while scanning customer " + err.Error())
+			return nil, errs.NewUnexpectedError("Unexpected database error")
+		}
+	}
 
-  if err != nil {
-    if err == sql.ErrNoRows {
-      return nil, errs.NewNotFoundError("Customer not found")
-    } else {
-      logger.Error("error while scanning customer " + err.Error())
-      return nil, errs.NewUnexpectedError("Unexpected database error")
-    }
-  }
-
-  return &c, nil
+	return &c, nil
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
-    client, err := sql.Open("mysql", "uppxago76tywvdd8:xwYZw66BhKm0lIAkJKn4@tcp(bvjh6qwrrq8wqlmgcp8s-mysql.services.clever-cloud.com:3306)/bvjh6qwrrq8wqlmgcp8s")
-    if err != nil {
-      panic(err)
-    }
-    // See "Important settings" section.
-    client.SetConnMaxLifetime(time.Minute * 3)
-    client.SetMaxOpenConns(10)
-    client.SetMaxIdleConns(10)
+	client, err := sqlx.Open("mysql", "uppxago76tywvdd8:xwYZw66BhKm0lIAkJKn4@tcp(bvjh6qwrrq8wqlmgcp8s-mysql.services.clever-cloud.com:3306)/bvjh6qwrrq8wqlmgcp8s")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
 
-    return CustomerRepositoryDb{client}
+	return CustomerRepositoryDb{client}
 }
